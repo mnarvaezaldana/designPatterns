983e559f97a0a46db7d2e52f3d92c53e0dd01452
MarcosArmando <narvaezaldana@gmail.com>
2022-03-23 19:17:47 -0600

    create Observer module for Observer pattern

---
+++ Observer/src/main/java/com/yucatancorp/observer/MyClass.java
@@ -0,0 +1,8 @@
+package com.yucatancorp.observer;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test for the Observer module");
+    }
+}
