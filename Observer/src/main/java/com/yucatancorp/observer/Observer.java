7fcaf448ad0a9340ad3606310a6c09e66b0821c8
MarcosArmando <narvaezaldana@gmail.com>
2022-03-26 11:50:46 -0600

    Comments and documentation added for Observer and Subject interfaces

--- Observer/src/main/java/com/yucatancorp/observer/Observer.java
+++ Observer/src/main/java/com/yucatancorp/observer/Observer.java
@@ -2,5 +2,14 @@ package com.yucatancorp.observer;
 
 public interface Observer {
 
+    /**
+     * First step:
+     *
+     *  Create the Observer interface and the method that is called when the subject is triggered
+     *  by the desired behavior.
+     *
+     *  next step is to create the interface Subject
+     */
+
     void update();
 }
--- Observer/src/main/java/com/yucatancorp/observer/Subject.java
+++ Observer/src/main/java/com/yucatancorp/observer/Subject.java
@@ -2,6 +2,20 @@ package com.yucatancorp.observer;
 
 public interface Subject {
 
+    /**
+     *
+     * Second step:
+     *
+     *  The second step is to create an interface that the subject will implements and it has three
+     *  methods: registerObserver which receive a Observer object (an object that implements Observer
+     *  interface. It also has another method that is called "removeObserver" that received an object
+     *  that implements also the Observer interface. And as well it has a method called notifyObservers
+     *  which will trigger the behavior needed for every observer listed in the class that implements
+     *  this interface.
+     *
+     * @param observer
+     */
+
     void registerObserver(Observer observer);
 
     void removeObserver(Observer observer);
