a78a75331e442ba0abbbbe154bd28d02a118199d
MarcosArmando <narvaezaldana@gmail.com>
2022-03-31 18:38:33 -0600

    complete decorator pattern

---
+++ decorator/src/main/java/com/yucatancorp/decorator/SteamedMilk.java
@@ -0,0 +1,18 @@
+package com.yucatancorp.decorator;
+
+public class SteamedMilk extends CondimentDecorator {
+
+    public SteamedMilk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .10;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Steamed Milk";
+    }
+}
