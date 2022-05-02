bbb46eadedfab651241929bf755f0a7b42b96a4a
MarcosArmando <narvaezaldana@gmail.com>
2022-05-02 18:19:47 -0500

    add test for factory method

--- FactoryMethod/src/main/java/com/yucatancorp/factorymethod/MyClass.java
+++ FactoryMethod/src/main/java/com/yucatancorp/factorymethod/MyClass.java
@@ -1,4 +1,21 @@
 package com.yucatancorp.factorymethod;
 
+import com.yucatancorp.factorymethod.pizzastores.CaliforniaPizzaStore;
+import com.yucatancorp.factorymethod.pizzastores.ChicagoPizzaStore;
+import com.yucatancorp.factorymethod.pizzastores.NYPizzaStore;
+import com.yucatancorp.factorymethod.pizzastores.PizzaStore;
+import com.yucatancorp.factorymethod.pizzatypes.Pizza;
+
 public class MyClass {
+
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoPizzaStore();
+        PizzaStore californiaStore = new CaliforniaPizzaStore();
+
+        Pizza pizzaOne = nyStore.orderPizza("cheese");
+        Pizza pizzaTwo = chicagoStore.orderPizza("veggie");
+        Pizza pizzaThree = californiaStore.orderPizza("clam");
+    }
+
 }
