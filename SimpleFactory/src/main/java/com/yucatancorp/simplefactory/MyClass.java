0336d98972ef2845f92ddae80e2fc86c70a630eb
MarcosArmando <narvaezaldana@gmail.com>
2022-05-02 17:01:28 -0500

    improve code methods for every pizza

--- SimpleFactory/src/main/java/com/yucatancorp/simplefactory/MyClass.java
+++ SimpleFactory/src/main/java/com/yucatancorp/simplefactory/MyClass.java
@@ -1,7 +1,5 @@
 package com.yucatancorp.simplefactory;
 
-import com.yucatancorp.simplefactory.pizzas.Pizza;
-
 public class MyClass {
 
     public static void main(String[] args) {
--- SimpleFactory/src/main/java/com/yucatancorp/simplefactory/pizzas/ClamPizza.java
+++ SimpleFactory/src/main/java/com/yucatancorp/simplefactory/pizzas/ClamPizza.java
@@ -3,21 +3,21 @@ package com.yucatancorp.simplefactory.pizzas;
 public class ClamPizza extends Pizza {
     @Override
     public void prepare() {
-        System.out.println("Prepare Pizza");
+        System.out.println("Prepare clam Pizza");
     }
 
     @Override
     public void cut() {
-        System.out.println("Cut Pizza");
+        System.out.println("Cut clam Pizza");
     }
 
     @Override
     public void box() {
-        System.out.println("Box Pizza");
+        System.out.println("Box clam Pizza");
     }
 
     @Override
     public void deliver() {
-        System.out.println("Deliver Pizza");
+        System.out.println("Deliver clam Pizza");
     }
 }
