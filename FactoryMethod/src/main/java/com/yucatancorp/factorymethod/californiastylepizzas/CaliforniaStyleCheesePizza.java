fa990093700d10196510a104fc34de967a9bbc21
MarcosArmando <narvaezaldana@gmail.com>
2022-05-02 17:45:59 -0500

    create california style pizzas and its own package

---
+++ FactoryMethod/src/main/java/com/yucatancorp/factorymethod/californiastylepizzas/CaliforniaStyleCheesePizza.java
@@ -0,0 +1,34 @@
+package com.yucatancorp.factorymethod.californiastylepizzas;
+
+import com.yucatancorp.factorymethod.pizzatypes.CheesePizza;
+
+public class CaliforniaStyleCheesePizza extends CheesePizza {
+
+    @Override
+    public void prepare() {
+        super.prepare();
+        System.out.println("California style");
+        System.out.println("--------");
+    }
+
+    @Override
+    public void cut() {
+        super.cut();
+        System.out.println("California style");
+        System.out.println("--------");
+    }
+
+    @Override
+    public void box() {
+        super.box();
+        System.out.println("California style");
+        System.out.println("--------");
+    }
+
+    @Override
+    public void deliver() {
+        super.deliver();
+        System.out.println("California style");
+        System.out.println("--------");
+    }
+}
